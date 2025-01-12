d85406e8af283dc5567ff000d6fe51d5f719c9f5
Kanishk Verma <kanishkverma00@gmail.com>
2025-01-12 15:09:44 +0530

    DecentralizedCloudStorage.sol

--- DecentralizedCloudStorage.sol
+++ DecentralizedCloudStorage.sol
@@ -2,52 +2,81 @@
 pragma solidity ^0.8.0;
 
 contract DecentralizedCloudStorage {
-    // Struct for storing file data
     struct File {
-        address owner;
-        string fileHash;
+        uint256 fileId;
+        address uploader;
         uint256 size;
-        uint256 uploadTime;
+        uint256 timestamp;
+        string encryptedIpfsHash;
+        uint256 amountPaid;
+        address[] authorizedUsers;
     }
 
-    // Mapping to store files by their unique ID
     mapping(uint256 => File) public files;
-    uint256 public fileCount;
 
-    // Event to notify about file uploads
-    event FileUploaded(
-        uint256 indexed fileId,
-        address indexed owner,
-        string fileHash,
-        uint256 size,
-        uint256 uploadTime
-    );
+    event FileUploaded(uint256 fileId, address uploader, uint256 size, string encryptedIpfsHash);
+    event PaymentMade(address indexed payer, uint256 indexed fileId, uint256 amount);
 
-    // Event to notify about file sharing
-    event FileShared(
-        uint256 indexed fileId,
-        address indexed owner,
-        address indexed recipient
-    );
+    uint256 private fileIdCounter;
 
-    // Function to upload a file
-    function uploadFile(string memory _fileHash, uint256 _size) public {
-        fileCount++;
-        files[fileCount] = File(msg.sender, _fileHash, _size, block.timestamp);
+    function uploadFile(uint256 _size, string memory _encryptedIpfsHash) public {
+        fileIdCounter++;
+        uint256 newFileId = fileIdCounter;
 
-        emit FileUploaded(fileCount, msg.sender, _fileHash, _size, block.timestamp);
+        files[newFileId] = File({
+            fileId: newFileId,
+            uploader: msg.sender,
+            size: _size,
+            timestamp: block.timestamp,
+            encryptedIpfsHash: _encryptedIpfsHash,
+            amountPaid: 0,
+            authorizedUsers: new address[](0) // Initialize with an empty array
+        });
+
+        emit FileUploaded(newFileId, msg.sender, _size, _encryptedIpfsHash);
     }
 
-    // Function to share a file
-    function shareFile(uint256 _fileId, address _recipient) public {
-        require(files[_fileId].owner == msg.sender, "You are not the owner of this file.");
-
-        emit FileShared(_fileId, msg.sender, _recipient);
-    }
-
-    // Function to pay for storage
     function payForStorage(uint256 _fileId, uint256 _amount) public payable {
-        require(msg.value == _amount, "Incorrect payment amount.");
-        // This is where you could implement logic for rewarding the storage provider
+        require(msg.value == _amount, "Incorrect payment amount");
+        require(files[_fileId].fileId != 0, "File not found");
+
+        files[_fileId].amountPaid += _amount;
+
+        emit PaymentMade(msg.sender, _fileId, _amount);
+    }
+
+    function addAuthorizedUser(uint256 _fileId, address _user) public {
+        require(msg.sender == files[_fileId].uploader, "Only the uploader can add authorized users");
+        files[_fileId].authorizedUsers.push(_user);
+    }
+
+    function isAuthorized(uint256 _fileId, address _user) public view returns (bool) {
+        address[] memory authorizedUsers = files[_fileId].authorizedUsers;
+        for (uint i = 0; i < authorizedUsers.length; i++) {
+            if (authorizedUsers[i] == _user) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function getFile(uint256 _fileId) public view returns (File memory) {
+        return files[_fileId];
+    }
+
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    function withdraw(uint256 _amount) public onlyOwner {
+        require(address(this).balance >= _amount, "Insufficient balance");
+        payable(owner).transfer(_amount);
+    }
+
+    constructor() {
+        owner = msg.sender;
     }
 }
